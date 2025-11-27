d898f8d51c154f351a4b9b12a355ae5dc6600b74
saanvigambhir083-ui <saanvigambhir083@gmail.com>
2025-11-27 09:41:14 +0530

    Add files via upload

---
+++ day60program110.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main() 
+{
+    int n, k;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements: ");
+    for (int i = 0; i < n; i++)
+     { scanf("%d", &arr[i]); }
+    printf("Enter size of subarray : ");
+    scanf("%d", &k);
+    printf("Max elements in each subarray of size %d are: ", k);
+    for (int i=0; i<=n-k;i++) 
+    {
+    int max=arr[i];
+    for (int j=1; j<k;j++)
+    {
+     if (arr[i+j]>max) 
+     {
+       max=arr[i+j];
+     }
+    }
+    printf("%d ", max);
+    }
+}
